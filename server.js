51319749b781c7e815ef5842bf35dd4459b60c83
Rutger-Jan Hoogerdijk <r.hoogerdijk@telegraafmedia.nl>
2017-05-21 11:45:37 +0200

    Added process file for pm2

--- server.js
+++ server.js
@@ -3,8 +3,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
-import { printSchema } from 'graphql/utilities/schemaPrinter';
+import {graphqlExpress, graphiqlExpress} from 'graphql-server-express';
+import {printSchema} from 'graphql/utilities/schemaPrinter';
 
 import schema from './data/schema';
 
@@ -13,7 +13,7 @@ const PORT = 3001;
 let graphQLServer = express().use('*', cors());
 const env = process.env.NODE_ENV;
 
-graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress ({
+graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({
   schema: schema,
 }));
 
@@ -21,12 +21,12 @@ if (env !== 'production') {
   graphQLServer.use('/graphiql', graphiqlExpress({
     endpointURL: '/graphql',
   }));
-}
 
-graphQLServer.use('/schema', (req, res) => {
-  res.set('Content-Type', 'text/plain');
-  res.send(printSchema(schema));
-});
+  graphQLServer.use('/schema', (req, res) => {
+    res.set('Content-Type', 'text/plain');
+    res.send(printSchema(schema));
+  });
+}
 
 graphQLServer.listen(PORT, () => console.log(
   `GraphQL Server is now running on https://localhost:${PORT}/graphql`
